b3a59de2569bf11270c30869b10a6b782056972d
aungNyeinChan93 <aungNyeinChan.dev@gmail.com>
2024-06-05 22:34:58 +0630

    tailwindcss tuto

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      color:{
+        primary:"red",
+
+      }
+    },
+  },
+  plugins: [],
+}
+
